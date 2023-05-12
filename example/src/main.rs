e1d01ceada0a7b74fc5267ef32e6d7aa460abac6
RobinCode <xv.robincode@gmail.com>
2023-05-12 13:53:10 +0800

    Add testing for new methods

--- example/src/main.rs
+++ example/src/main.rs
@@ -50,6 +50,7 @@ fn main() {
         result.button_id, result.radio_button_id, result.checked,
     );
 
+    show_dynamic_text_dialog();
     show_process_bar_marquee();
     show_process_bar();
 
@@ -57,11 +58,42 @@ fn main() {
     show_msg_dialog("Title", "!!!", "Error", TD_ERROR_ICON);
 }
 
+// Show dynamic text dialog
+fn show_dynamic_text_dialog() {
+    let mut conf = TaskDialogConfig {
+        window_title: "Dynamic Text".to_string(),
+        main_instruction: "Main".to_string(),
+        content: "Content".to_string(),
+        footer: "Footer".to_string(),
+        expanded_information: "Info".to_string(),
+        ..Default::default()
+    };
+
+    let conf_ptr: *mut TaskDialogConfig = &mut conf;
+    let conf_long_ptr = conf_ptr as isize;
+    thread::spawn(move || unsafe {
+        let conf = std::mem::transmute::<isize, *mut TaskDialogConfig>(conf_long_ptr);
+        for i in 1..4 {
+            thread::sleep(Duration::from_secs(1));
+            (*conf).set_main_instruction(format!("Main {}", i).as_str());
+            (*conf).set_content(format!("Content {}", i).as_str());
+            (*conf).set_footer(format!("Footer {}", i).as_str());
+            (*conf).set_expanded_information(format!("Info {}", i).as_str());
+        }
+        (*conf).set_main_instruction("Main !!!");
+        (*conf).set_content("Content !!!");
+        (*conf).set_footer("Footer !!!");
+        (*conf).set_expanded_information("Info !!!");
+    });
+
+    show_task_dialog(&mut conf).unwrap();
+}
+
 // Process Bar Marquee
 fn show_process_bar_marquee() {
     let mut conf = TaskDialogConfig {
         window_title: "Process Bar".to_string(),
-        main_instruction: "Process Bar Marquee".to_string(),
+        main_instruction: "Process Bar Marquee 1".to_string(),
         ..Default::default()
     };
     conf.enable_process_bar(true);
@@ -73,8 +105,10 @@ fn show_process_bar_marquee() {
         let conf = std::mem::transmute::<isize, *mut TaskDialogConfig>(conf_long_ptr);
         (*conf).set_process_bar_marquee(true, 100);
         thread::sleep(Duration::from_secs(5));
+        (*conf).set_main_instruction("Process Bar Marquee 2");
         (*conf).set_process_bar_marquee(true, 1);
         thread::sleep(Duration::from_secs(3));
+        (*conf).set_main_instruction("Process Bar Marquee Stop");
         (*conf).set_process_bar_marquee(false, 1);
     });
 
@@ -86,6 +120,7 @@ fn show_process_bar() {
     let mut conf = TaskDialogConfig {
         window_title: "Process Bar".to_string(),
         main_instruction: "Process Bar (Not Marquee)".to_string(),
+        content: "".to_string(),
         ..Default::default()
     };
     conf.enable_process_bar(false);
